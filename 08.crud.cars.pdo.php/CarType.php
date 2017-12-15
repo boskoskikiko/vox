be6ac03acafe720db0b1c36fb41333ebaeff11e5
Kiko <boskoskikiko@mail.com>
2017-12-15 17:58:19 +0100

    Update CarType.php

--- 08.crud.cars.pdo.php/CarType.php
+++ 08.crud.cars.pdo.php/CarType.php
@@ -5,8 +5,14 @@ require_once 'Model.php';
 
 class CarType extends Model {
 
+	private $id;
 	private $name;
 	private $power;
+	private $image;
+
+	public function getId(){
+		return $this->id;
+	}
 
 	public function getName() {
 		return $this->name;
@@ -14,6 +20,10 @@ class CarType extends Model {
 
 	public function getPower() {
 		return $this->power;
+	}
+
+	public function getImage() {
+		return $this->image;
 	}
 
 	public function setName($name) {
@@ -25,6 +35,11 @@ class CarType extends Model {
 		$this->power = $power;
 		return $this;
 	}
+	public function setImage($image) {
+		 $this->image = $image;
+		 return $this;
+	}
+
 
 	public function __construct($rows = [])
 	{
@@ -35,7 +50,61 @@ class CarType extends Model {
 
 
 
-	static private $tableName = "carmodel";
+	static private $tableName = "cartype";
+
+
+
+//==============SAVE / UPDATE-INSERT ==Method
+
+	public function save(){
+		
+		if (isset($this->id)) {
+					
+			//====Update
+			try {
+				
+				$db = Connection::connect();
+
+				$stmt = $db->prepare("UPDATE ".self::$tableName." 
+		      	SET name=:name,
+					power=:power,
+					image=:image
+		      	 WHERE id=:id");
+
+				// var_dump($this->ime);
+			
+
+				$stmt->bindParam(':name', $this->name);
+				$stmt->bindParam(':power',$this->power);
+				$stmt->bindParam(':image',$this->image);
+
+				$stmt->bindParam(':id', $this->id);
+				$stmt->execute();
+			} catch (PDOException $e) {
+					echo $sql . "<br>" . $e->getMessage();
+			}
+		}else{
+
+			//====Insert
+			try {
+				
+				$db = Connection::connect();
+
+				$stmt = $db->prepare("INSERT INTO cartype (name, power, image) VALUES (:name, :power, :image)");
+				
+				$stmt->bindParam(':name',$this->name);
+				$stmt->bindParam(':power',$this->power);
+				$stmt->bindParam(':image',$this->image);
+				//die(var_dump($this));
+				$stmt->execute();
+				echo "New Car created Seccessfull";
+
+			} catch (PDOException $e) {
+				
+					echo $sql . "<br>" . $e->getMessage();
+			}
+		}//end ifelse
+	}//end function save
 
 }
 
