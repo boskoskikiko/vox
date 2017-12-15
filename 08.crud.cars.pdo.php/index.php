5a6c6ce56a227a25ae0ed8ff38c3e980911e83d5
Kiko <boskoskikiko@mail.com>
2017-12-15 17:54:03 +0100

    Update index.php

--- 08.crud.cars.pdo.php/index.php
+++ 08.crud.cars.pdo.php/index.php
@@ -1,43 +1,52 @@
-<?php 
-
 require_once "CarModel.php";
-
-// $car = new CarModel();
-// $car->setIme("Opeqweqwel");
-// $car->setRegistracija("PP21qweqwe4EW");
-// $car->setGodina("201qweqwe4");
-// $car->save();
-
+require_once "CarType.php";
 
 $car = CarModel::getAll();
+// $car = CarType::getAll();
 
-		// if (isset($_GET['del'])) {
-		// 	 CarModel::deleteByID($_GET['del']);
-		// }else{
-
-		// }
 ?>
-<table border="1">
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+	<title>title</title>
+</head>
+<body>
+	<header>
+		<div class="nav">
+			<a href="index.php">POCETNA</a> |
+			<a href="Add.php">ADD CAR MARK</a> |
+			<a href="cartypes/index.php">VIEW CAR MODEL</a> <hr>
+		</div>
+	</header>
 
-<?php	foreach ($car as $kay => $value) { ?>
+	<table border="1">
+
+	<?php	foreach ($car as $kay => $value) { ?>
 
 		<tr>
 				<td> <a href=""><?php echo $value->getId();?></a></td>
-				<td> <a href="index.php?id=<?=$value->getId()?>"><?php echo $value->getIme();?></a></td>
+				<td> <a href="profilCar.php?id=<?=$value->getId()?>"><?php echo $value->getIme();?></a></td>
 				<td> <a href=""><?php echo $value->getRegistracija();?></a></td>
 				<td> <a href=""><?php echo $value->getGodina();?></a></td>
+				<td> <a href=""><?php echo $value->gettest();?></a></td>
+				<td> <a href=""><?php echo $value->getid_cartype();?></a></td>
 				<td> <a href="edit.php?id=<?=$value->getId()?>">Edit</a></td>
 				<td> <a href="delete.php?id=<?=$value->getId()?>">Delete</a></td>
 		</tr>
 
-<?php	} ?>
+	<?php	} ?>
 
 </table>
- <a href="Add.php">add</a>
+
+
+</body>
+</html>
+
+
+
 
 <?php
 
-// $car = CarModel::deleteByID(3);
-
-// echo $car;
  ?>
